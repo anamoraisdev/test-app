bcbb47f2ec3f83596b4afaa5dbb206f51c337b78
Ana Karolina Morais <anakarolinamorais@MacBook-Air-de-Ana.local>
2024-11-12 19:20:43 -0300

    create basic react app with expo

---
+++ App.js
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>NomeApp</Text>
+    </View>
+  );
+}
+
+
